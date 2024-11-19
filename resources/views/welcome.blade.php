ccc745fe987e267533a2c17da3d07005308f3f79
Morozov Maxim <morozovmrom@gmail.com>
2024-11-19 18:20:20 +0100

    DESIGN: Add info about generated work to main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,23 @@
 <x-main-page-layout title="Morozov Maksim Personal Page"> 
+
+    @foreach($works as $work)
+    <div class="mt-4">
+        <h2 class="font-bold text-lg">{{$work->company}}</h2>
+        <h3 class="font-bold text-lg">{{$work->title}}</h3>
+        <div>
+            From
+            {{ \Carbon\Carbon::parse($work->start_date)->format('Y-m') }}
+            to
+            @if($work->end_date != null)
+                {{  \Carbon\Carbon::parse($work->end_date)->format('Y-m') }}
+            @else
+                present
+            @endif 
+
+        </div>
+        <p class="text-sm">{{$work->description}}</p>
+    </div>
+    @endforeach
+
     Hello world
 </x-main-page-layout>
