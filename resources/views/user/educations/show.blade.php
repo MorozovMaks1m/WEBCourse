27736af0bd6596800629de2fe6c51cb53230c22d
Morozov Maxim <morozovmrom@gmail.com>
2024-11-21 09:55:59 +0100

    BUGFIX: Possibility to edit no thesis education

--- resources/views/user/educations/show.blade.php
+++ resources/views/user/educations/show.blade.php
@@ -22,6 +22,7 @@
     </div>
 
     <div class="mt-4">
+        @if ($education->thesis)
         <h4 class="text-md font-semibold text-gray-800">Thesis: {{ $education->thesis->title }}</h4>
         <ul class="list-disc list-inside mt-2 text-gray-700">
             <div>
@@ -31,5 +32,6 @@
                 <span class="bg-gray-200 text-gray-800 px-2 py-1 rounded-full text-xs">{{$skill->name}}</span>
             @endforeach    
         </ul>
+        @endif
     </div>
 </x-main-page-layout>
